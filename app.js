83bfde1244f8d6c28bdcdc01c6e85768a74511b9
tjlefebvre <tjlefebvre@outlook.com.au>
2025-12-01 23:45:49 +1100

    Added all Craftsy course lesson info

--- app.js
+++ app.js
@@ -50,7 +50,43 @@ const courses = [
         "platform": "Craftsy",
         "status": "Not Started",
         "link": "https://www.craftsy.com/class/essential-techniques-for-sketching-the-energy-of-places",
-        "lessons": []
+        "lessons": [
+    {
+        "number": 1,
+        "title": "Class Preview (1:36)",
+        "link": "https://www.craftsy.com/class/essential-techniques-for-sketching-the-energy-of-places"
+    },
+    {
+        "number": 2,
+        "title": "Finding the Perfect Place (13:11)",
+        "link": "https://www.craftsy.com/class/essential-techniques-for-sketching-the-energy-of-places"
+    },
+    {
+        "number": 3,
+        "title": "Drawing People in the City (19:50)",
+        "link": "https://www.craftsy.com/class/essential-techniques-for-sketching-the-energy-of-places"
+    },
+    {
+        "number": 4,
+        "title": "Buildings as Backdrop (20:52)",
+        "link": "https://www.craftsy.com/class/essential-techniques-for-sketching-the-energy-of-places"
+    },
+    {
+        "number": 5,
+        "title": "The Entourage (20:52)",
+        "link": "https://www.craftsy.com/class/essential-techniques-for-sketching-the-energy-of-places"
+    },
+    {
+        "number": 6,
+        "title": "Adding Color (22:36)",
+        "link": "https://www.craftsy.com/class/essential-techniques-for-sketching-the-energy-of-places"
+    },
+    {
+        "number": 7,
+        "title": "Hitting the Streets: On Location (19:03)",
+        "link": "https://www.craftsy.com/class/essential-techniques-for-sketching-the-energy-of-places"
+    },
+],
     },
     {
         "title": "Sketching the City in Pen, Ink & Watercolor",
@@ -58,7 +94,48 @@ const courses = [
         "platform": "Craftsy",
         "status": "Not Started",
         "link": "https://www.craftsy.com/class/sketching-the-city-in-pen-ink-watercolor",
-        "lessons": []
+        "lessons": [
+    {
+        "number": 1,
+        "title": "Class Preview (1:35)",
+        "link": "https://www.craftsy.com/class/sketching-the-city-in-pen-ink-watercolor"
+    },
+    {
+        "number": 2,
+        "title": "Thumbnails & Composition (19:46)",
+        "link": "https://www.craftsy.com/class/sketching-the-city-in-pen-ink-watercolor"
+    },
+    {
+        "number": 3,
+        "title": "Drawing Facades (28:53)",
+        "link": "https://www.craftsy.com/class/sketching-the-city-in-pen-ink-watercolor"
+    },
+    {
+        "number": 4,
+        "title": "Urban Texture (29:05)",
+        "link": "https://www.craftsy.com/class/sketching-the-city-in-pen-ink-watercolor"
+    },
+    {
+        "number": 5,
+        "title": "Doors & Windows (23:18)",
+        "link": "https://www.craftsy.com/class/sketching-the-city-in-pen-ink-watercolor"
+    },
+    {
+        "number": 6,
+        "title": "Details That Give Cities Life (25:10)",
+        "link": "https://www.craftsy.com/class/sketching-the-city-in-pen-ink-watercolor"
+    },
+    {
+        "number": 7,
+        "title": "Light & Shadow (22:35)",
+        "link": "https://www.craftsy.com/class/sketching-the-city-in-pen-ink-watercolor"
+    },
+    {
+        "number": 8,
+        "title": "Panoramic Cityscapes (26:30)",
+        "link": "https://www.craftsy.com/class/sketching-the-city-in-pen-ink-watercolor"
+    },
+],
     },
     {
         "title": "Perspective for Sketchers",
@@ -66,7 +143,48 @@ const courses = [
         "platform": "Craftsy",
         "status": "Not Started",
         "link": "https://www.craftsy.com/class/perspective-for-sketchers",
-        "lessons": []
+        "lessons": [
+    {
+        "number": 1,
+        "title": "Class Preview (1:31)",
+        "link": "https://www.craftsy.com/class/perspective-for-sketchers"
+    },
+    {
+        "number": 2,
+        "title": "Measuring Accurate Proportions (16:44)",
+        "link": "https://www.craftsy.com/class/perspective-for-sketchers"
+    },
+    {
+        "number": 3,
+        "title": "Building a Sketch in Layers (20:30)",
+        "link": "https://www.craftsy.com/class/perspective-for-sketchers"
+    },
+    {
+        "number": 4,
+        "title": "Concepts of Perspective Space (23:07)",
+        "link": "https://www.craftsy.com/class/perspective-for-sketchers"
+    },
+    {
+        "number": 5,
+        "title": "Perspective Angles (22:34)",
+        "link": "https://www.craftsy.com/class/perspective-for-sketchers"
+    },
+    {
+        "number": 6,
+        "title": "Using Watercolor (20:37)",
+        "link": "https://www.craftsy.com/class/perspective-for-sketchers"
+    },
+    {
+        "number": 7,
+        "title": "Location Sketching: Interior (13:32)",
+        "link": "https://www.craftsy.com/class/perspective-for-sketchers"
+    },
+    {
+        "number": 8,
+        "title": "Location Sketching: Streetscape (14:08)",
+        "link": "https://www.craftsy.com/class/perspective-for-sketchers"
+    },
+],
     },
     {
         "title": "Travel Sketching in Mixed Media",
@@ -74,7 +192,48 @@ const courses = [
         "platform": "Craftsy",
         "status": "Not Started",
         "link": "https://www.craftsy.com/class/travel-sketching-in-mixed-media",
-        "lessons": []
+        "lessons": [
+    {
+        "number": 1,
+        "title": "Class Preview (1:25)",
+        "link": "https://www.craftsy.com/class/travel-sketching-in-mixed-media"
+    },
+    {
+        "number": 2,
+        "title": "Single-Line Sketching (30:25)",
+        "link": "https://www.craftsy.com/class/travel-sketching-in-mixed-media"
+    },
+    {
+        "number": 3,
+        "title": "Draw Like a Painter (29:36)",
+        "link": "https://www.craftsy.com/class/travel-sketching-in-mixed-media"
+    },
+    {
+        "number": 4,
+        "title": "Tinting Drawings in Watercolor (27:21)",
+        "link": "https://www.craftsy.com/class/travel-sketching-in-mixed-media"
+    },
+    {
+        "number": 5,
+        "title": "Sketch Collecting (20:51)",
+        "link": "https://www.craftsy.com/class/travel-sketching-in-mixed-media"
+    },
+    {
+        "number": 6,
+        "title": "Painless Perspective (23:59)",
+        "link": "https://www.craftsy.com/class/travel-sketching-in-mixed-media"
+    },
+    {
+        "number": 7,
+        "title": "Watercolor Sketching (21:06)",
+        "link": "https://www.craftsy.com/class/travel-sketching-in-mixed-media"
+    },
+    {
+        "number": 8,
+        "title": "Working in Layers (16:26)",
+        "link": "https://www.craftsy.com/class/travel-sketching-in-mixed-media"
+    },
+],
     },
     {
         "title": "Sketching Landscapes in Pen, Ink & Watercolor",
@@ -82,7 +241,48 @@ const courses = [
         "platform": "Craftsy",
         "status": "Not Started",
         "link": "https://www.craftsy.com/class/sketching-landscapes-in-pen-ink-watercolor",
-        "lessons": []
+        "lessons": [
+    {
+        "number": 1,
+        "title": "Class Preview (1:42)",
+        "link": "https://www.craftsy.com/class/sketching-landscapes-in-pen-ink-watercolor"
+    },
+    {
+        "number": 2,
+        "title": "Shari's Sketchbooks (22:23)",
+        "link": "https://www.craftsy.com/class/sketching-landscapes-in-pen-ink-watercolor"
+    },
+    {
+        "number": 3,
+        "title": "Composition (13:33)",
+        "link": "https://www.craftsy.com/class/sketching-landscapes-in-pen-ink-watercolor"
+    },
+    {
+        "number": 4,
+        "title": "Skies (36:46)",
+        "link": "https://www.craftsy.com/class/sketching-landscapes-in-pen-ink-watercolor"
+    },
+    {
+        "number": 5,
+        "title": "Big Shapes (31:52)",
+        "link": "https://www.craftsy.com/class/sketching-landscapes-in-pen-ink-watercolor"
+    },
+    {
+        "number": 6,
+        "title": "Texture (33:43)",
+        "link": "https://www.craftsy.com/class/sketching-landscapes-in-pen-ink-watercolor"
+    },
+    {
+        "number": 7,
+        "title": "Adding Depth With Value (15:38)",
+        "link": "https://www.craftsy.com/class/sketching-landscapes-in-pen-ink-watercolor"
+    },
+    {
+        "number": 8,
+        "title": "Expressing Unique Moments (21:39)",
+        "link": "https://www.craftsy.com/class/sketching-landscapes-in-pen-ink-watercolor"
+    },
+],
     },
     {
         "title": "Sketching People in Motion",
@@ -90,7 +290,48 @@ const courses = [
         "platform": "Craftsy",
         "status": "Not Started",
         "link": "https://www.craftsy.com/class/sketching-people-in-motion",
-        "lessons": []
+        "lessons": [
+    {
+        "number": 1,
+        "title": "Class Preview (0:53)",
+        "link": "https://www.craftsy.com/class/sketching-people-in-motion"
+    },
+    {
+        "number": 2,
+        "title": "The Pencil Gesture (16:42)",
+        "link": "https://www.craftsy.com/class/sketching-people-in-motion"
+    },
+    {
+        "number": 3,
+        "title": "Adding Ink (31:00)",
+        "link": "https://www.craftsy.com/class/sketching-people-in-motion"
+    },
+    {
+        "number": 4,
+        "title": "Brushwork & Hatching (20:27)",
+        "link": "https://www.craftsy.com/class/sketching-people-in-motion"
+    },
+    {
+        "number": 5,
+        "title": "Adding Color (26:01)",
+        "link": "https://www.craftsy.com/class/sketching-people-in-motion"
+    },
+    {
+        "number": 6,
+        "title": "Advanced Techniques: Water-Soluble Ink (19:07)",
+        "link": "https://www.craftsy.com/class/sketching-people-in-motion"
+    },
+    {
+        "number": 7,
+        "title": "Advanced Techniques: Direct to Watercolor (20:28)",
+        "link": "https://www.craftsy.com/class/sketching-people-in-motion"
+    },
+    {
+        "number": 8,
+        "title": "Capturing Multiple People (15:12)",
+        "link": "https://www.craftsy.com/class/sketching-people-in-motion"
+    },
+],
     } 
 ];
 
